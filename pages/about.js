e19050a1ee2b0d7e37359acf8a0f3a827edf63eb
synoce-iitgn <76461801+aman1chaudhary@users.noreply.github.com>
2023-09-23 13:36:12 +0530

    23seo

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,9 @@ import pic5 from '../public/images/our-team/pic5.jpg';
 import pic6 from '../public/images/our-team/pic6.jpg';
 import tree from '../public/images/tree.jpg';
 import Link from 'next/link';
-
+import googleScholarImg from "../public/images/social-icons/google-scholar.jpg"
+import linkedinImg from "../public/images/social-icons/linkedin.jpg"
+import researchGateImg from "../public/images/social-icons/research-gate.jpg"
 
 const TeamData = [
     {
@@ -22,6 +24,8 @@ const TeamData = [
         about: "Dr. Harsh Shah is founder and Chief Executive Officer at AgroCast Analytics Pvt. Ltd. He leads Agriculture, Water and Climate risk sectors. Before starting AgroCast, Dr. Shah worked as a research associate in the Monsoon Mission Project at Indian Institute of Technology Gandhinagar. He was selected as an alternative sponsored fellow at PNNL, USA. Dr. Shah has published more than 15 research papers in internationally reviewed journals and also presented work in national/international conferences. He is actively working as hydrological modeler in the ISIMIP project. He also contributed to a book chapter published in the COP24 meeting and part of Gujarat State Action Plan on Climate Change (SAPCC) report. Dr. Shah has an intricate knowledge of various physics based hydrological models (VIC, SWAT and CWatM), stemming from his time spent working on hydrological models ever since his Ph.D. Dr. Shah has evaluated hydrological model to analyze long-term change in water budget across Indian sub-continental river basins. Dr. Shah is skillful in the evaluation of climate change and its impact on the water budget.",
         BackgroundInformation: ["Research Associate at IIT Gandhinagar", "Ph.D. in Civil Engineering (IIT), Gandhinagar", "Bachelor in Civil Engineering (Gold Medal)", "Harsh is native in Gujarati and fluent in Hindi and English"],
         LinkedIn: "https://www.linkedin.com/in/harsh-shah-77595179/",
+        GoogleScholar: "https://scholar.google.co.in/citations?user=P744FjcAAAAJ&hl=en",
+        ResearchGate:"https://www.researchgate.net/profile/Harsh-Shah-12"
     },
     {
         id: "member_2",
@@ -32,6 +36,8 @@ const TeamData = [
         BackgroundInformation: [],
         about: "Dr. Vimal Mishra, currently a professor in Civil Engineering and Earth Sciences at In- dian Institute of Technology Gandhinagar, is one of the new the GLASS Panel mem- bers. Prior to joining IIT Gandhinagar, he completed his Ph.D. from Purdue Uni- versity and postdoctoral fellowship from University of Washington, Seattle. Dr. Mishra's research focuses on large scale hy- drological modeling, remote sensing, and climate change im- pact assessment. He is a fellow of the American Geophysical Union (AGU) and the National Academy of Sciences, India (NASI). He is currently serving as an editor of Earth's Future and associate editor of Journal of Hydrology.",
         LinkedIn: "https://www.linkedin.com/in/vimal-mishra-21162265/",
+        GoogleScholar: "https://scholar.google.co.in/citations?hl=en&user=wq7CgpUAAAAJ",
+        ResearchGate:"https://www.researchgate.net/profile/Vimal-Mishra-2"
     },
     {
         id: "member_3",
@@ -42,6 +48,8 @@ const TeamData = [
         BackgroundInformation: ["Bachelor in commerce (B.com)", "Shaily  is native in Gujarati and fluent in Hindi and English", "Mrs. Shaily possesses in-depth knowledge of Tally ERP, a renowned accounting software, ensuring meticulous management and representation of financial data."],
         about: "Mrs. Shaily D. Shah serves as the esteemed Director and CFO of our firm, bringing to the table her robust educational background with a B. Com and an MBA in Finance from Gujarat University. Her ascension to the directorship came during a challenging period following the unfortunate passing of Shri. Love K. Shah, the former Director. Demonstrating resilience and intelligent leadership, Mrs. Shaily has adeptly assumed responsibility for the company's auditing operations, upholding transparency, adherence to standards, and financial accountability. She is well-versed with a comprehensive array of commerce-related courses, encompassing areas from financial accounting and taxation to business law and economics, fortifying the company's financial and operational backbone. Her profound understanding of finance, combined with her unwavering dedication, solidifies the financial foundation of our enterprise, guiding us forward with vision and integrity.",
         LinkedIn: "https://www.linkedin.com/in/shaily-shah-mba-finance-591b9221b/",
+        GoogleScholar: "",
+        ResearchGate:""
     },
     {
         id: "member_4",
@@ -53,6 +61,8 @@ const TeamData = [
             "Meen is native in Gujarati and fluent in Hindi and English"],
         about: "Mr. Meenkumar Patel is a dynamic Junior Civil Engineer at AgroCast Analytics Pvt. Ltd, with a specialized expertise in the Surveying and GIS sector. Proficient in deploying advanced techniques like DGPS and Drone Surveys, Mr. Patel seamlessly integrates cutting-edge technology with traditional GIS-GPS methodologies to deliver precise and efficient survey outcomes. His foundational internship experiences at GWSSB and hands-on involvement in bridge construction sites during his Bachelor's degree have endowed him with a holistic understanding of the field. His commitment to innovation and continuous learning ensures our projects stand out in quality and precision. A true asset to our team, Meenkumar consistently elevates our standard of work, bringing unmatched expertise and vision to every venture.",
         LinkedIn: "https://www.linkedin.com/in/meen-patel-37a687286/",
+        GoogleScholar: "",
+        ResearchGate:""
     },
     {
         id: "member_5",
@@ -67,6 +77,8 @@ const TeamData = [
             "Azaz is native in Gujarati and fluent in Hindi and English"],
         about: "Dr. Azaz Pathan is Senior Hydrologist at AgroCast Analytics Pvt. Ltd. He leads  GIS based flood risk assessment, mappings, and hydrological sectors. Before starting AgroCast, Dr. Azaz worked as a Senior Research Fellow in the Indo-Netherlands Project at Indian Institute of Technology Gandhinagar. He has delivered hands-on training (flood modeling) at different university of India and abroad as an expert. Dr. Azaz has published more than 30 research papers in internationally reviewed journals/conference and also presented work in national/international conferences. Dr. Azaz has an intricate knowledge of various software including HEC-RAS, HEC-HMS, SWAT, Arc-GIS, QGIS, SWMM, WaterGEM, QGIS. His area of interests are flood inundation mapping (1D/2D), Flood Risk mapping using MCDM techniques, Flood control measures, Flood resilience strategies, Water Conservation, Water Harvesting, Dam Break Analysis, Dam Safety management, Water Distribution System, Integrated Water Management, Hydrologic modeling.",
         LinkedIn: "https://www.linkedin.com/in/dr-azazkhan-i-pathan-25398870/",
+        GoogleScholar: "",
+        ResearchGate:""
     },
 
     {
@@ -78,6 +90,8 @@ const TeamData = [
         Email: "mehtapreet1514@gmail.com",
         about: "Mr. Preet Mehta is a proficient Junior Civil Engineer at AgroCast Analytics Pvt. Ltd, showcasing profound expertise in Storm Water Management, Image Processing, Surveying and GIS domain. Proficient in harnessing advanced tools, including DGPS, Drones, and GIS for detailed surveying, Mr. Mehta also holds a distinctive proficiency in image processing techniques, bringing added layers of depth and precision to projects. Further elevating his credentials, he possesses a valid DGCA Drone Pilot license, reflecting not just his expertise in flying drones but also his commitment to adhering to industry standards and safety. An indispensable member of our team, Preet's multifaceted skill set ensures that our projects remain at the forefront of innovation, accuracy, and comprehensive solutions.",
         LinkedIn: "https://www.linkedin.com/in/preet-mehta-b61998284/",
+        GoogleScholar: "",
+        ResearchGate:""
     },
 
 ]
@@ -88,21 +102,21 @@ const AboutPage = () => {
 
     const handleMemberClick = (id) => {
         const isMobile = window.innerWidth < 767;
-      
+
         if (selectedMember === id && isMobile) {
-          setSelectedMember(null); // Deselect the member if already selected
+            setSelectedMember(null); // Deselect the member if already selected
         } else {
-          setSelectedMember(id);
-          // Scroll to the member's details if it's a mobile device
-          if (isMobile) {
-            memberDetailsRef.current.scrollIntoView({
-              behavior: 'smooth',
-              block: 'start',
-            });
-          }
+            setSelectedMember(id);
+            // Scroll to the member's details if it's a mobile device
+            if (isMobile) {
+                memberDetailsRef.current.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                });
+            }
         }
-      };
-      
+    };
+
 
     const handleClose = () => {
         setSelectedMember(null);
@@ -155,7 +169,7 @@ const AboutPage = () => {
                                 key={data.id}
                                 onClick={() => handleMemberClick(data.id)}
                             >
-                                <Image src={data.images} alt='team member'/>
+                                <Image src={data.images} alt='team member' />
                                 <div className="tree_item_content">
                                     <h4>{data.name}</h4>
                                     <p>{data.role}</p>
@@ -167,7 +181,7 @@ const AboutPage = () => {
                     <div ref={memberDetailsRef}>
                         {selectedMember && (
                             <div className='member_details_container'>
-                                
+
 
                                 <div className="row member_details" >
                                     <div className="col-lg-4 col-md-4 col-sm-12">
@@ -178,18 +192,22 @@ const AboutPage = () => {
                                     </div>
                                     <div className='col-lg-8 col-md-8 col-sm-12'>
                                         <div className="member_content">
-                                        <button className='popup_close' title='close window' onClick={handleClose}><FaTimes /></button>
+                                            <button className='popup_close' title='close window' onClick={handleClose}><FaTimes /></button>
                                             <h4 className="name">{TeamData.find((member) => member.id === selectedMember).name}</h4>
                                             <p className="designation">{TeamData.find((member) => member.id === selectedMember).role}</p>
                                             <span className="about">{TeamData.find((member) => member.id === selectedMember).about}</span>
-                                            <div className="footer_social_icons">
-                                                <a href={TeamData.find((member) => member.id === selectedMember).LinkedIn} target='blank' className="social_icons linkedin"><i className="fa fa-linkedin"></i></a>
-                                                {/* <a href={TeamData.find((member) => member.id === selectedMember).Email} className="social_icons twitter"><i className="fa fa-envelope"></i></a> */}
+                                            <div className="people_social_icons">
+                                                <a href={TeamData.find((member) => member.id === selectedMember).LinkedIn} target='blank'><Image src={linkedinImg} /></a>
+                                                {TeamData.find((member) => member.id === selectedMember).GoogleScholar && (
+                                                    <a href={TeamData.find((member) => member.id === selectedMember).GoogleScholar} target='blank'><Image src={googleScholarImg} /></a>
+                                                )}
+                                                {TeamData.find((member) => member.id === selectedMember).ResearchGate && (
+                                                    <a href={TeamData.find((member) => member.id === selectedMember).ResearchGate} target='blank'><Image src={researchGateImg} /></a>
+                                                )}
                                             </div>
-
-
                                         </div>
                                     </div>
+
                                 </div>
                             </div>
                         )}
--- pages/research-articles.js
+++ pages/research-articles.js
@@ -1,5 +1,21 @@
 import React from 'react'
 import Head from 'next/head';
+import Image from 'next/image';
+import Img1 from "../public/images/publications/1.jpeg"
+import Img2 from "../public/images/publications/2.jpeg"
+import Img3 from "../public/images/publications/3.jpeg"
+import Img4 from "../public/images/publications/4.jpeg"
+import Img5 from "../public/images/publications/5.jpeg"
+import Img6 from "../public/images/publications/6.jpeg"
+import Img7 from "../public/images/publications/7.jpeg"
+import Img8 from "../public/images/publications/8.jpeg"
+import Img9 from "../public/images/publications/9.jpeg"
+import Img10 from "../public/images/publications/10.jpeg"
+import Img11 from "../public/images/publications/11.jpeg"
+import Img12 from "../public/images/publications/12.jpeg"
+import Img13 from "../public/images/publications/13.jpeg"
+import Img14 from "../public/images/publications/14.jpeg"
+import Img15 from "../public/images/publications/15.jpeg"
 
 const ResearchArticlesData = [
     {
@@ -7,79 +23,94 @@ const ResearchArticlesData = [
         ArticleLink: "https://journals.ametsoc.org/view/journals/hydr/17/10/jhm-d-15-0231_1.xml?tab_body=abstract-display",
         ArticleAuthors: ["Vimal Mishra", "Harsh L. Shah"],
         PublicationYear: "2016",
+        ArticleImage: Img1
     },
     {
         ArticleName: "Hydroclimatological Perspective of the Kerala Flood of 2018",
         ArticleLink: "https://link.springer.com/content/pdf/10.1007/s12594-018-1079-3.pdf",
         ArticleAuthors: ["Vimal Mishra", "Harsh L. Shah"],
         PublicationYear: "2018",
+        ArticleImage: Img2
     },
     {
         ArticleName: "Uncertainty and Bias in Satellite-Based Precipitation Estimates over Indian Subcontinental Basins: Implications for Real-Time Streamflow Simulation and Flood Prediction",
         ArticleLink: "https://journals.ametsoc.org/view/journals/hydr/17/2/jhm-d-15-0115_1.xml?tab_body=abstract-display",
         ArticleAuthors: ["Vimal Mishra", "Harsh L. Shah"],
         PublicationYear: "2016",
+        ArticleImage: Img3
     }, {
         ArticleName: "Climate Change Impacts on Streamflow in India",
         ArticleLink: "https://www.researchgate.net/profile/Harsh-Shah-12/publication/330158561_Climate_Change_Impacts_on_Streamflow_in_India/links/5c303a08299bf12be3ae4d64/Climate-Change-Impacts-on-Streamflow-in-India.pdf",
         ArticleAuthors: ["Vimal Mishra", "Harsh L. Shah"],
         PublicationYear: "2018",
+        ArticleImage: Img4
     }, {
         ArticleName: "Reconstruction of droughts in India using multiple land-surface models (1951–2015)",
         ArticleLink: "https://hess.copernicus.org/articles/22/2269/2018/",
         ArticleAuthors: ["Vimal Mishra", "Reepal Shah", "Syed Azhar", "Harsh Shah", "Parth Modi", "Rohini Kumar"],
         PublicationYear: "2018",
+        ArticleImage: Img5
     }, {
         ArticleName: "Projected Increase in Hydropower Production in India under Climate Change",
         ArticleLink: "https://www.nature.com/articles/s41598-018-30489-4",
         ArticleAuthors: ["Syed Azhar Ali", "Saran Aadhar", "Harsh L. Shah", "Vimal Mishra "],
         PublicationYear: "2018",
+        ArticleImage: Img6
     }, {
         ArticleName: "Propagation of forcing and model uncertainties on to hydrological drought characteristics in a multi-model century-long experiment in large river basins",
         ArticleLink: "https://link.springer.com/article/10.1007/s10584-016-1778-y",
         ArticleAuthors: ["L. Samaniego", "R. Kumar", "L. Breuer", "A. Chamorro", "M. Flörke", "I. G. Pechlivanidis", "D. Schäfer", "H. Shah", "T. Vetter", "M. Wortmann", "X. Zeng "],
         PublicationYear: "2017",
+        ArticleImage: Img7
     }, {
         ArticleName: "Strong Influence of Irrigation on Water Budget and Land Surface Temperature in Indian Subcontinental River Basins",
         ArticleLink: "https://agupubs.onlinelibrary.wiley.com/doi/pdf/10.1029/2018JD029132",
         ArticleAuthors: ["Harsh L. Shah", "Tian Zhou", "Maoyi Huang", "Vimal Mishra"],
         PublicationYear: "2019",
+        ArticleImage: Img8
     }, {
         ArticleName: "Multimodel assessment of sensitivity and uncertainty of evapotranspiration and a proxy for available water resources under climate change",
         ArticleLink: "https://link.springer.com/article/10.1007/s10584-016-1886-8",
         ArticleAuthors: ["Vimal Mishra", "Rohini Kumar", "Harsh L. Shah", "Luis Samaniego", "S. Eisner", "Tao Yang", "Climatic Change "],
         PublicationYear: "2017",
+        ArticleImage: Img9
     }, {
         ArticleName: "Contrasting influence of human activities on agricultural and hydrological droughts in India",
         ArticleLink: "https://www.researchgate.net/profile/Vimal-Mishra-2/publication/348931428_Contrasting_influence_of_human_activities_on_agricultural_and_hydrological_droughts_in_India/links/6017ea2345851517ef2f1814/Contrasting-influence-of-human-activities-on-agricultural-and-hydrological-droughts-in-India.pdf",
         ArticleAuthors: ["Harsh L. Shah", "Deep Shah", "Vimal Mishra"],
         PublicationYear: "2021",
+        ArticleImage: Img10
     }, {
         ArticleName: "Understanding each other's models: an introduction and a standard representation of 16 global water models to support intercomparison, improvement, and communication",
         ArticleLink: "https://gmd.copernicus.org/articles/14/3843/2021/",
         ArticleAuthors: ["Camelia-Eliza Telteu", "Hannes Müller Schmied", "Wim Thiery", "Guoyong Leng", "Peter Burek", "Xingcai Liu", "Julien Eric Stanislas Boulange", "Lauren Seaby Andersen", "Manolis Grillakis", "Simon Newland Gosling", "Yusuke Satoh", "Oldrich Rakovec", "Tobias Stacke", "Jinfeng Chang", "Niko Wanders, Harsh Lovekumar Shah, Tim Trautmann, Ganquan Mao, Naota Hanasaki, Aristeidis Koutroulis, Yadu Pokhrel", "Luis Samaniego", "Yoshihide Wada", "Vimal Mishra", "Junguo Liu", "Petra Döll", "Fang Zhao", "Anne Gädeke", "Sam S. Rabin", "Florian Herz"],
         PublicationYear: "2021",
+        ArticleImage: Img11
     }, {
         ArticleName: "Impacts of hydrological model calibration on projected hydrological changes under climate change—a multi-model assessment in three large river basins",
         ArticleLink: "https://link.springer.com/article/10.1007/s10584-020-02872-6",
         ArticleAuthors: ["Shaochun Huang", "Harsh Shah", "Bibi S. Naz", "Narayan Shrestha", "Vimal Mishra", "Prasad Daggupati", "Uttam Ghimire", "Tobias Vetter"],
         PublicationYear: "2020",
+        ArticleImage: Img12
     }, {
         ArticleName: "Roles of Irrigation and Reservoir Operations in Modulating Terrestrial Water and Energy Budgets in the Indian Subcontinental River Basins",
         ArticleLink: "https://agupubs.onlinelibrary.wiley.com/doi/pdfdirect/10.1029/2019JD031059",
         ArticleAuthors: ["Harsh L. Shah", "Tian Zhou", "Ning Sun", "Maoyi Huang", "Vimal Mishra"],
         PublicationYear: "2019",
+        ArticleImage: Img13
     }, {
         ArticleName: "Does comprehensive evaluation of hydrological models influence projected changes of mean and high flows in the Godavari River basin?",
         ArticleLink: "https://link.springer.com/article/10.1007/s10584-020-02847-7",
         ArticleAuthors: ["Vimal Mishra", "Harsh Shah", "M. Rocío Rivas López", "Anastasia Lobanova", "Valentina Krysanova"],
         PublicationYear: "2020",
+        ArticleImage: Img14
     },
     {
         ArticleName: "Multimodel assessment of water budget in Indian sub-continental river basins",
         ArticleLink: "https://www.sciencedirect.com/science/article/pii/S0022169421010271",
         ArticleAuthors: ["Anuj Prakash Kushwaha", "Amar Deep Tiwari", "Swarup Dangar", "Harsh Shah", "Shanti Shwarup Mahto", "Vimal Mishra"],
         PublicationYear: "2021",
+        ArticleImage: Img15
     },
 
 
@@ -109,27 +140,20 @@ const ResearchArticles = () => {
 
 
 
+                    <div className='row' >
+                        {ResearchArticlesData.map((article, index) => (
+                            <div className="col-md-4" key={index}>
+                                <div className='articles_card'>
+                                    <Image src={article.ArticleImage} />
 
-                    {ResearchArticlesData.map((article, index) => (
-                        <div className="services_card_container" key={index}>
-                            <div className="services_card_content">
-                                <h3>{article.ArticleName}</h3>
-                                <p><strong>Authors: </strong>{article.ArticleAuthors.map((author, index) => (
-                                    <>{author}, </>
-                                ))} </p>
-
-                                <div className='card_btn'>
-                                    <a href={article.ArticleLink} target='blank'><button>Read Article</button></a>
+                                    <h3>{article.ArticleName}</h3>
+                                    <div className='card_btn'>
+                                        <a href={article.ArticleLink} target='blank'><button>Read Article</button></a>
+                                    </div>
                                 </div>
-
-
-
                             </div>
-
-                        </div>
-
-
-                    ))}
+                        ))}
+                    </div>
 
 
                 </div>
